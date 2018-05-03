c44fd1a2791bd51f06c2393e5fce2f00f44ce467
eldesh <nephits@gmail.com>
2018-05-02 23:54:50 -0700

    format

--- make_plugin.sh
+++ make_plugin.sh
@@ -2,14 +2,14 @@
 
 if [ ! -f "plugin/extraction/astCommon.ml" ]
 then
-    sh clean_extraction.sh
-else
-    a=`stat --format="%Z" theories/Extraction/AstCommon.ml`
-    b=`stat --format="%Z" plugin/extraction/astCommon.ml`
-    if [ "$a" -gt "$b" ]
-	then
 	sh clean_extraction.sh
-    fi
+else
+	a=`stat --format="%Z" theories/Extraction/AstCommon.ml`
+	b=`stat --format="%Z" plugin/extraction/astCommon.ml`
+	if [ "$a" -gt "$b" ]
+	then
+		sh clean_extraction.sh
+	fi
 fi
 
 cd plugin
